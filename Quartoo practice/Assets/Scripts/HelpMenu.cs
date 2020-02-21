99d66ebb9f5be1d1fe4f5afa8935ca16ebaf85ad
Courtney Campbell <hull.e.courtney@gmail.com>
2020-02-21 13:31:37 -0600

    add image gallery

--- Quartoo practice/Assets/Scripts/HelpMenu.cs
+++ Quartoo practice/Assets/Scripts/HelpMenu.cs
@@ -1,8 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelpMenu : MonoBehaviour
 {
+    public Sprite[] images;
+    public Image currentImage;
+    public Button nextImage;
+    public Button previousImage;
+    public int i = 0;
 
+    void Start()
+    {
+        updatePanel();
+    }
+
+    public void next()
+    {
+        if (i + 1 < images.Length)
+        {
+            i++;
+            updatePanel();
+        }
+
+    }
+
+    public void previous()
+    {
+        if (i - 1 >= 0)
+        {
+            i--;
+            updatePanel();
+        }
+    }
+
+    private void updatePanel()
+    {
+        currentImage.sprite = images[i];
+    }
 }
