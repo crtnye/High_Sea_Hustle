de1e21fe365dc0a1fd14f9c09d95eb7ac8d14d28
Courtney Campbell <hull.e.courtney@gmail.com>
2020-04-02 14:03:04 -0500

    Add function headers for username input and toggles

--- Quartoo practice/Assets/Scripts/PlayerSelectionPanelController.cs
+++ Quartoo practice/Assets/Scripts/PlayerSelectionPanelController.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerSelectionPanelController : MonoBehaviour
 {
+
+    public InputField usernameInput;
+
+
+    private string username = "Player1";
+    private bool userGoesFirst = true;
+    private bool easyAI = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,26 @@ public class PlayerSelectionPanelController : MonoBehaviour
     {
         
     }
+
+    public void playButton()
+    {
+        //Not sure if this is the right way, but need some kind of check to provide a default name
+        if (usernameInput.text != "")
+            username = usernameInput.text;
+
+        //Save the username and other relevant data somewhere
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void firstMoveToggled()
+    {
+        userGoesFirst = !userGoesFirst;
+    }
+
+    public void AIDifficultyToggled()
+    {
+        easyAI = !easyAI;
+    }
+
 }
